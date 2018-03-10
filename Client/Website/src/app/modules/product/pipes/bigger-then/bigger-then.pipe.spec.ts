8c38bc3aa735c4f4644ec7c3d8fd43d6eeb2bfc1
Nitzan Bar <nitzantar@gmail.com>
2018-03-10 12:25:39 +0200

    deleted all spec.ts files...

--- Client/Website/src/app/modules/product/pipes/bigger-then/bigger-then.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BiggerThenPipe } from './bigger-then.pipe';
-
-describe('BiggerThenPipe', () => {
-  it('create an instance', () => {
-    const pipe = new BiggerThenPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- Client/Website/src/app/modules/product/pipes/smaller-then/smaller-then.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SmallerThenPipe } from './smaller-then.pipe';
-
-describe('SmallerThenPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SmallerThenPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
