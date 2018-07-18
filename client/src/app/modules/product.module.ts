b2c3ccd5fce50e94160c61832a170aa295befa58
Nitzan <nitz@nitz.com>
2018-07-18 19:46:22 +0300

    deleted pipes

--- client/src/app/modules/product.module.ts
+++ client/src/app/modules/product.module.ts
@@ -4,16 +4,11 @@ import { ProductDetailsComponent } from 'app/components/product/product-details/
 import { AddOrUpdateProductComponent } from 'app/components/product/add-or-update-product/add-or-update-product.component';
 import { ProductsListComponent } from 'app/components/product/products-list/products-list.component';
 import { PaginationComponent } from 'app/components/product/pagination/pagination.component';
-import { CategoryPipe } from 'app/pipes/category.pipe';
-import { PricePipe } from 'app/pipes/price.pipe';
-import { NamePipe } from 'app/pipes/name.pipe';
 import { CheapestProdctByCategoryComponent } from 'app/components/product/cheapest-prodct-by-category/cheapest-prodct-by-category.component';
 import { PriceHistoryChartWithD3jsComponent } from 'app/components/product/price-history-chart-with-d3js/price-history-chart-with-d3js.component';
 import { BarChartProuctPriceComponent } from 'app/components/product/bar-chart-prouct-price/bar-chart-prouct-price.component';
 import { PreferredProductComponent } from 'app/components/product/preferred-product/preferred-product.component';
 import { ProductDetailsPreviewComponent } from 'app/components/product/product-details-preview/product-details-preview.component';
-import { BiggerThenPipe } from 'app/pipes/bigger-then.pipe';
-import { SmallerThenPipe } from 'app/pipes/smaller-then.pipe';
 import { ProductsListFilterComponent } from 'app/components/product/products-list-filter/products-list-filter.component';
 import { StatisticsMainPageComponent } from 'app/components/product/statistics-main-page/statistics-main-page.component';
 import { FormsModule } from '@angular/forms';
@@ -28,16 +23,11 @@ import { AppRouting } from '../app.routing';
     AddOrUpdateProductComponent,
     ProductsListComponent,
     PaginationComponent,
-    CategoryPipe,
-    PricePipe,
-    NamePipe,
     CheapestProdctByCategoryComponent,
     PriceHistoryChartWithD3jsComponent,
     BarChartProuctPriceComponent,
     PreferredProductComponent,
     ProductDetailsPreviewComponent,
-    BiggerThenPipe,
-    SmallerThenPipe,
     ProductsListFilterComponent,
     StatisticsMainPageComponent,
   ],
